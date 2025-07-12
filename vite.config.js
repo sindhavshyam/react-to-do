9506ae457187462350ca4830844e564c52811280
{sindhavshyam} <sindhavs22@gmail.com>
2025-07-12 13:02:58 +0530

    refixed error

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:process.env.VITE.BASE.PATH || "/react-to-do"
+  base: (process.env.VITE_BASE_PATH) ? process.env.VITE_BASE_PATH : "/react-to-do"
 })
